69ac514b9e1a016cadda11fbcaf1cb6359fea440
BOMBARDELLA BASTIEN p1935610 <bastien.bombardella@etu.univ-lyon1.fr>
2021-06-07 13:54:22 +0000

    Comment controller

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Comment;
+use App\Form\CommentFormType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CommentController extends AbstractController
 {
@@ -16,5 +19,47 @@ class CommentController extends AbstractController
         return $this->render('comment/index.html.twig', [
             'controller_name' => 'CommentController',
         ]);
+    }
+
+    public function show(int $id): Response {
+        $commentRepository = $this->getDoctrine()->getRepository(Comment::class);
+        $comment = $commentRepository->find($id);
+
+        if (!$comment) {
+            throw $this->createNotFoundException(
+                "Pas de Comment trouvé avec l'id \"${id}\""
+            );
+        }
+
+        return $this->render('comment/show.html.twig', [
+            'post' => $post
+        ]);
+    }
+
+    public function create(Request $request): Response {
+        $comment = new Comment();
+
+        $form = $this->createForm(CommentFormType::class, $comment);
+        $form->handleRequest();
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $comment = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($comment);
+            $entityManager->flush();
+        }
+
+        return $this->render('comment/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    public function edit(): Response {
+
+    }
+
+    public function remove(): Response {
+
     }
 }
